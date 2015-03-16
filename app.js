a39714fe9a49f7f59b74593f34e0e34946331c64
Yadav Bhattarai <yadavb@gmail.com>
2015-03-15 22:03:26 -0700

    adding post and put routs

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.send('hello world get');
 });
  
+app.post('/', function (req, res) {
+  res.send('hello world post');
+});
+
+app.put('/', function (req, res) {
+  res.send('hello world put');
+});
+
 app.listen(process.env.PORT || 5000);
  
 module.exports = app;
